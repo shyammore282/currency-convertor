f7588a8022302ad8bdd3aad5166618e0f83a646e
shyam more <skumarmoure@gmail.com>
2024-08-13 14:59:08 +0530

    add custom-probressBar

---
+++ src/components/custom-progressBar/ProgressBar.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./style.css";
+
+const ProgressBar = () => {
+  const [percentage, setPercentage] = useState(0);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const onChangeHandler = (e) => {
+    if (e.target.value > 100 && e.target.value < 0) {
+      setPercentage(e.target.value);
+      setErrorMsg("please enter the value less then 100");
+    } else {
+      setErrorMsg("");
+      setPercentage(e.target.value);
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <div className="progressbar">
+            {percentage >= 0 && percentage <= 100 ? (
+              <div
+                className="percentageBar"
+                style={{ width: `${percentage}%` }}
+              >
+                {percentage}
+              </div>
+            ) : (
+              <p>{errorMsg}</p>
+            )}
+          </div>
+          <div className="input-value">
+            <label htmlFor="input">input percentage</label>
+            <input
+              className="input"
+              onChange={onChangeHandler}
+              type="number"
+              value={percentage}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProgressBar;
